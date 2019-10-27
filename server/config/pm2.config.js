2592bac22db89d3e7c4003185a73e16669b0ff23
kevin flynn <flynny85@gmail.com>
2019-10-27 12:50:18 +0000

    tweaking.

--- server/config/pm2.config.js
+++ server/config/pm2.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   apps : [{
+  	name : "app-is-it-up",
     script : "app.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    },    
     autorestart: true,
     watch : true,
     max_memory_restart: '150M',
