c1ba4687cc77af74de707dea06793e294d15f4a4
kevin flynn <flynny85@gmail.com>
2019-10-30 22:37:10 +0000

    Latest build files.

---
+++ front/src/data/vars.js
@@ -0,0 +1,6 @@
+const vars = {
+	active : [true,false],
+	Ping : [15,30,45,60],
+	Method : ['GET','POST','PUT','DELETE'],
+}
+export default vars;
---
+++ server/data/vars.js
@@ -0,0 +1,6 @@
+
+const vars = {
+	Ping : [15,30,45,60],
+	Method : ['GET','POST','PUT','DELETE'],
+}
+exports.vars = vars;
